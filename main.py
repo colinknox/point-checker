cd01407f83d4b017d9bb8df865c4613d4f00f4fb
colinknox <colinwknox@gmail.com>
2026-02-10 20:23:15 -0500

    added 2 methods for Point class

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ class Point:
         self.x = x
         self.y = y
 
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+
 class Rectangle:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+
+point_one = Point(5, 4)
+
+print(point_one.x)
+print(point_one.y)
+print(point_one.get_x())
+print(point_one.get_y())
